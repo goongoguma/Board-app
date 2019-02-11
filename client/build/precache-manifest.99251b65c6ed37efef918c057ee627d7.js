0416fc6f7b20e8b1b36f3aa70f3127d16fddd980
goongoguma <wogus7an@gmail.com>
2019-02-11 18:05:02 +0900

    Build upload

---
+++ client/build/precache-manifest.99251b65c6ed37efef918c057ee627d7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f6bd982718f794ce1aed",
+    "url": "/static/js/main.f6bd9827.chunk.js"
+  },
+  {
+    "revision": "c696ffb8e5033b1ed2f5",
+    "url": "/static/js/1.c696ffb8.chunk.js"
+  },
+  {
+    "revision": "abaf05239cf2d0c9efd92f25f23f879c",
+    "url": "/index.html"
+  }
+];
